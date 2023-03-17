4bd5bd0643e2a4b807dcce9946124b626c78f731
Swapnpran <swapnil.dhondge@gmail.com>
2023-03-17 13:34:15 +0530

    first commit demo..

---
+++ src/com/demo/TestDemo.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class TestDemo {
+	
+		public static void main(String[] args) {
+		String hungry ="yes";
+		if (hungry== "yes")
+		System.out.println("eat something....");
+		else {
+		System.out.println("Do work.......");
+		}
+		}
+}
